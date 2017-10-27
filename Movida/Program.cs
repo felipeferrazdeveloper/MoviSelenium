ea837e74b06174cace5004d92b80aaf07d158a90
Cctavio <oc.ld.jf@gmail.com>
2017-10-27 01:30:34 -0200

    Updating: Navegar pelo menu até a tela de 'Parcerias'

--- Movida/Program.cs
+++ Movida/Program.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +14,20 @@ namespace Movida
     {
         static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver();
+            IWebDriver driver = new FirefoxDriver();
             driver.Manage().Window.Maximize(); 
             var tempo = TimeSpan.FromSeconds(20);
             driver.Manage().Timeouts().ImplicitWait = tempo;
 
-            driver.Navigate().GoToUrl("http://www.movida.com.br"); //Não sei porquê, mas precsa navegar até essa URL antes para a próxima funcionar
-            //driver.Navigate().GoToUrl("http://www.movida.com.br/parcerias"); 
+            driver.Navigate().GoToUrl("http://www.movida.com.br"); 
+            
+            //Clica no Menu principal
+            driver.FindElement(By.ClassName("cd-dropdown-trigger")).Click();
 
-            driver.FindElement(By.ClassName("cd-dropdown-trigger")).Click();             
-            //driver.FindElement(By.XPath("//div[@class='cd-dropdown-wrapper']//nav[@class='cd-dropdown']//ul[@class='cd-dropdown-content']//li[@class='has-children']//a[@title='A Movida']")).Click();
+            Actions mouseMove = new Actions(driver);
+            mouseMove.MoveToElement(driver.FindElement(By.XPath("//div[@class= 'cd-dropdown-wrapper']/*/ul//a[@title='A Movida']"))).Build().Perform();
+            driver.FindElement(By.XPath("//div[@class= 'cd-dropdown-wrapper']//ul//a[@title='A Movida']/following-sibling::ul//a[@title='Parcerias']")).Click();
+
             driver.Close();
         }
     }
